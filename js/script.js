6c0709bcaf3ac1528af7bf7d2d2a25632365916d
Arkadiusz Cichosz <cichorek87220@gmail.com>
2017-03-15 18:09:36 +0100

    add buttons left right functionality in js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,33 @@
 $(function(){
 	const carouselList = $('#carousel ul');
+	const leftButton = $('.left-button');
+	const rightButton = $('.right-button');
+
+	leftButton.click(changeSlideLeft);
+
+	rightButton.click(changeSlideRight);
+
 	function moveFirstSlide() {
 		let firstItem = carouselList.find('li:first');
 		let lastItem = carouselList.find('li:last');
 		lastItem.after(firstItem);
-		carouselList.css({marginLeft: 0});
+		carouselList.css({marginLeft: -300});
 	}
 
-	function changeSlide() {
-		carouselList.animate({'marginLeft': -300}, 500, moveFirstSlide);
+	function moveLastSlide() {
+		let firstItem = carouselList.find('li:first');
+		let lastItem = carouselList.find('li:last');
+		firstItem.before(lastItem);
+		carouselList.css({marginLeft: -300});
 	}
 
-	setInterval(changeSlide, 3000);
+	function changeSlideLeft() {
+		carouselList.animate({'marginLeft': -600}, 500, moveFirstSlide);
+	}
+
+	function changeSlideRight() {
+		carouselList.animate({'marginLeft': 0}, 500, moveLastSlide);
+	}
+
+	setInterval(changeSlideLeft, 5000); // or changeSlideRight
 });
